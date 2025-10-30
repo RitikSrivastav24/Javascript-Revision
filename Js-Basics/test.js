9a76217658e454deabe70d9e5544486cab43c220
Ritik Shrivastav <118468793+Rititk24@users.noreply.github.com>
2025-10-30 05:17:17 +0000

    configuration file done.

---
+++ Js-Basics/test.js
@@ -0,0 +1,14 @@
+const empId= 54123;
+let empName="Ritik";
+
+/*Avoid using var because it overwrites the originbal value 
+var empEmail="ritik@gmail.com"
+var empEmail="nitin@gmail.com"
+*/
+//now let's chnage values 
+/* empId=4561;
+ i got an error :- Assignment to constant variable. It means we cannot change a value of a variable declared with const. It is not allowed in Javascript
+*/
+empName="Nitin"
+console.log(empName);
+
