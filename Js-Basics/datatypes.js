1fb034579c2ad19d772ee9d766eefaa8acc5b2ee
Ritik Shrivastav <118468793+RitikSrivastav24@users.noreply.github.com>
2025-10-31 09:57:40 +0000

    datatypes

---
+++ Js-Basics/datatypes.js
@@ -0,0 +1,31 @@
+//There are two types of datatypes in JS
+//Primitive : 7 types. String,Number,Boolean,null,undefined,Symbol,BigInt
+const score=100
+const isLoggedIn=true
+
+const temp=null
+
+let empName;
+const empId=Symbol('5564')
+const empId1=Symbol('5564')
+console.log(empId==empId1)
+
+//const bigNumber=5651549541219451445614n
+//Reference Type(Non-Primitive)
+//Array
+const students =["Rohan","Ankit","Nikhil"]
+//Object
+const myObj ={
+    name:"Test",
+    id:55,
+    email:"abc@gmail.com"
+}
+//Functions
+
+const myFunction=function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber)
+
+console.log(typeof temp)
