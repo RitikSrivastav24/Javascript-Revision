f47c124c5f6a660fdd353ed34e133918678f29e1
Ritik Shrivastav <118468793+RitikSrivastav24@users.noreply.github.com>
2025-11-04 14:28:22 +0000

    Date and time is complex topic

---
+++ Js-Basics/date.js
@@ -0,0 +1,20 @@
+let myDate=new Date()
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+// console.log(typeof myDate)
+
+//code to get define exact date
+let mydefinedDate= new Date(2025, 0,7)
+// let mydefinedDate= new Date(2025, 0,7,7,8)
+// let mydefinedDate= new Date("04-12-2025")
+// console.log(mydefinedDate.toLocaleString())
+
+let myTimeStamp=Date.now()
+// console.log(myTimeStamp)
+// console.log(mydefinedDate.getTime())
+// console.log(Math.floor((Date.now())/1000))
+
+let newDate=new Date()
+console.log(newDate.getDay())
