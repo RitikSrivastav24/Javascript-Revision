d67f5a9273236d4a219dc5fbd9b6da93f74538eb
Ritik Shrivastav <118468793+RitikSrivastav24@users.noreply.github.com>
2025-12-15 07:17:26 +0000

    questions

--- Js-Basics/comparison.js
+++ Js-Basics/comparison.js
@@ -1,33 +1,57 @@
-/*This is a normal comparison 
-console.log(2>1);
-console.log(2<1);
-console.log(2==1)
-*/
+    /*This is a normal comparison 
+    console.log(2>1);
+    console.log(2<1);
+    console.log(2==1)
+    */
 
-/*problem is that when we compare different datatypes.In this javascript
-//automatically converts string into int but sometimes it does not give
-predictable result. So it is better before comparing must check values have
-same datatypes.*/
-console.log("4" > 3);
+    /*problem is that when we compare different datatypes.In this javascript
+    //automatically converts string into int but sometimes it does not give
+    predictable result. So it is better before comparing must check values have
+    same datatypes.*/
+    console.log("4" > 3);
+
+    /*
+    console.log("7"== 4)
+    console.log("7" === 4)
+    in above code console.log("7" == 4);
+
+    "7" is a string, 4 is a number.
+
+    The == operator tries to convert "7" → 7 (number).
+
+    Then it compares 7 == 4, which is false.
+
+    So, output → false
+
+    console.log("7" === 4);
+
+    "7" (string) and 4 (number) have different types.
+
+    The === operator does not do type conversion.
+
+    Since type differs, it’s false immediately.
+    */
+
+
 
 /*
-console.log("7"== 4)
-console.log("7" === 4)
-in above code console.log("7" == 4);
+Q.1 What is the difference between == and === operators in JavaScript,
+and why is it recommended to use one over the other? 
+Ans- ==(Loose Equality)
+Compares values after type coercion.
+Different types can be converted to the same type before comparison.
 
-"7" is a string, 4 is a number.
+===(strict equality)
+compares values and types before type conversion
+Both type and value must be identical for it to return true.
 
-The == operator tries to convert "7" → 7 (number).
+key points :- Always prefer === for predictable results and to 
+avoid unexpected bugs due to type coercion.
 
-Then it compares 7 == 4, which is false.
-
-So, output → false
-
-console.log("7" === 4);
-
-"7" (string) and 4 (number) have different types.
-
-The === operator does not do type conversion.
-
-Since type differs, it’s false immediately.
+Q.2 What are the logical operators in JavaScript, and how do they work?
+Ans- Logical operators are used to combine or invert Boolean values.
+JavaScript has three main logical operators:
+&& (AND) → Returns the first falsy value or the last truthy value.
+|| (OR) → Returns the first truthy value or the last falsy value.
+! (NOT) → Inverts the Boolean value.
 */
