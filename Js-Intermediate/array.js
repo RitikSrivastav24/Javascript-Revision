7391ce252491886a5742c8a1241b324f6ae3cbc5
Ritik Shrivastav <118468793+RitikSrivastav24@users.noreply.github.com>
2025-12-16 06:31:02 +0000

    added 5 common interview questions.

--- Js-Intermediate/array.js
+++ Js-Intermediate/array.js
@@ -5,8 +5,8 @@ const myArray2=new Array(1,2,3,4,5) //Differnent ways to array
 //console.log(myArray[0])
 
 //Array Methods
-// myArray.push(7) It add a value in existing array
-// myArray.pop(8) It removes a value from existing array
+//myArray.push(7) //It add a value in existing array in the end
+myArray.pop(8) //It removes a value from existing array in the end 
 
 // myArray.unshift(4)
 // myArray.shift(4)
@@ -19,11 +19,11 @@ const myArray2=new Array(1,2,3,4,5) //Differnent ways to array
 // console.log(newArray)
 
 const myarr1= myArray.slice(1,3)
-console.log(myarr1)
-console.log("B",myArray)
+ console.log(myarr1)
+// console.log("B",myArray)
 const myarr2=myArray.splice(1,3)
-console.log(myarr2)
-console.log("B",myArray)
+// console.log(myarr2)
+// console.log("B",myArray)
 
 //slice is a copy machine,
 //splice is a surgeon who cuts and removes stuff from the original array.
@@ -42,11 +42,48 @@ console.log("B",myArray)
 // const n2=arr1.forEach(n=>n*2) //.forEach() just runs a function on each item. It does not return a new array. We use it for doing something, like printing or updating UI, etc
 // console.log(n2)
 
-// const arr = [5, 10, 15, 20];
-// const filtered = arr.filter(num => num > 10);
-// console.log(filtered);
-// console.log(arr)
+ const arr = [5, 10, 15, 20];
+ const filtered = arr.filter(num => num > 10);
+ console.log(filtered);
+ console.log(arr)
 
 const items=["a","b","c","d"]
 items.length=1
-console.log(items)
+// console.log(items)
+
+/*
+Q.1 What is an array in JavaScript, and how is it different from an 
+    object? What kind of data can an array store? How are elements 
+    accessed?
+Ans- An Array is a data-structure is used to store multiple values into
+    single variable.Arrays can store any type of data in JavaScript like 
+    (numbers, strings, objects, functions, even mixed). Array elements 
+    are accessed using index numbers, starting from 0.
+
+    example :- const arr = [10, "JS", true, { role: "dev" }];
+               console.log(arr[0]); // 10
+
+Q.2 What is difference between array.push() and array.pop()?
+Ans- Push() adds one or more element to the end of the array whereas 
+Pop() removes the last element.
+
+example :- const arr = [1, 2, 3];
+           arr.push(4); // [1, 2, 3, 4]
+           arr.pop();  // [1, 2, 3]
+      Key point:- Both methods modify the original array.
+
+Q.3 Difference between slice() and splice()?
+Ans- slice() returns a portion of an array without modifying it, 
+     whereas splice() modifies the original array by adding or removing
+     elements
+
+Q.4 What is the difference between map() and forEach()?Return value?
+When should you use map() instead of forEach()?
+Ans- forEach is used for looping and does not return anything while map
+     retuens a new array after transforming element.
+
+Q.5 What is filter() and why it is used ?
+Ans- filter() is used to apply a condition on an array and returns a 
+     new array containing only the elements that satisfy the condition.
+     It does not modify the original array.
+ */
